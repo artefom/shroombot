4aa83af90db327cc0ca17f8fa7d79b85320497eb
Artyom Fomenko <artyom.fomenko@distribusion.com>
2025-03-28 19:40:34 +0100

    Update chat name

--- shroombot/telegram_test.py
+++ shroombot/telegram_test.py
@@ -27,7 +27,7 @@ async def test_telegram_api():
     ) as client:
         telegram = LiveTelegramApi(client)
 
-        admin_chat = await get_chat_id(client, "meanddadya")
+        admin_chat = await get_chat_id(client, "gribni_tsa_bot")
 
         await telegram.send_message(admin_chat, MyTextMessage("Hello, world"))
 
